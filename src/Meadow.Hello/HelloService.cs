c2535dba9ffabcbb99e4c99ce7ae2ba6093465d6
Brian Kim <lamebrain@gmail.com>
2018-06-11 10:02:18 -0700

    initial commit

---
+++ src/Meadow.Hello/HelloService.cs
@@ -0,0 +1,13 @@
+using System;
+using Microsoft.SPOT;
+
+namespace Meadow.Hello
+{
+    public class HelloService
+    {
+        public void SayHello()
+        {
+            Debug.Print("Hello, I'm Meadow.");
+        }
+    }
+}
